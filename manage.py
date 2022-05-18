90718ed91192c325af8b63b737ba9977742049d7
Mathenge-Alex <arxmaina@gmail.com>
2022-05-18 21:49:59 +0300

    feat: add app run module

---
+++ manage.py
@@ -0,0 +1,27 @@
+from app import create_app,db
+from flask_script import Manager,Server
+from  flask_migrate import Migrate, MigrateCommand
+from app.models import User
+
+
+app = create_app('development')
+
+manager = Manager(app)
+migrate = Migrate(app,db)
+
+manager.add_command('server', Server)
+manager.add_command('db',MigrateCommand)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User )
+
+@manager.command
+def test():
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+if __name__ == '__main__':
+    manager.run()
